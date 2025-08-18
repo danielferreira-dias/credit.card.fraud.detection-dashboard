d151016908be9bac9a1a6716d2ac43383274cce4
Daniel Dias <danieldias.it98@gmail.com>
2025-08-18 23:31:59 +0100

    added dockerfile, project organization

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from app.schemas.transaction_schema import Transaction
 
 app = FastAPI()
 
---
+++ backend/app/schemas/transaction_schema.py
@@ -0,0 +1,41 @@
+from pydantic import BaseModel
+
+class Transaction(BaseModel):
+    channel_large: int
+    channel_medium: int
+    device_Android_App: int
+    device_Safari: int
+    device_Firefox: int
+    USD_converted_total_amount: float
+    device_Chrome: int
+    device_iOS_App: int
+    city_Unknown_City: int
+    country_USA: int
+    country_Australia: int
+    country_Germany: int
+    country_UK: int
+    country_Canada: int
+    country_Japan: int
+    country_France: int
+    device_Edge: int
+    country_Singapore: int
+    channel_mobile: int
+    country_Nigeria: int
+    country_Brazil: int
+    country_Russia: int
+    country_Mexico: int
+    is_off_hours: int
+    max_single_amount: float
+    USD_converted_amount: float
+    channel_web: int
+    is_high_amount: int
+    is_low_amount: int
+    transaction_hour: int
+    hour: int
+    device_NFC_Payment: int
+    device_Magnetic_Stripe: int
+    device_Chip_Reader: int
+    high_risk_transaction: int
+    channel_pos: int
+    card_present: int
+    distance_from_home: float
