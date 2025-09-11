a55733964f4ca5e32ccef70a1491fac99faa7c90
Daniel Dias <danieldias.it98@gmail.com>
2025-09-11 17:53:47 +0100

    Started card design

--- frontend/src/components/StatsCard.tsx
+++ frontend/src/components/StatsCard.tsx
@@ -1,6 +1,6 @@
 export default function StatsCard( { typeStat, statValue } : { typeStat : string, statValue : number } )  {
     return (
-        <div className="flex flex-row bg-[#0F0F11]">
+        <div className="flex flex-row bg-[#0F0F11] w-[100%] h-40 rounded-2xl justify-center items-center">
             {typeStat}, {statValue}
         </div>
     )
--- frontend/src/pages/Transactions.tsx
+++ frontend/src/pages/Transactions.tsx
@@ -1,5 +1,17 @@
+import StatsCard from "../components/StatsCard";
+
 export default function TransactionPage(){
+    const currentStat : string = "Transações Fraudulentas"
+    const currentValue : number = 1247
+
     return (
-        <div className="flex h-full w-full text-white justify-center items-center">Transaction Page</div>
+        <div className="flex flex-col h-full w-full text-white items-center p-4">
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 w-full gap-4 items-center">
+                <StatsCard typeStat={currentStat} statValue={currentValue} />
+                <StatsCard typeStat={currentStat} statValue={currentValue} />
+                <StatsCard typeStat={currentStat} statValue={currentValue} />
+                <StatsCard typeStat={currentStat} statValue={currentValue} />
+            </div>
+        </div>
     )
 }
